2ae3c95e224b0a393c51b3e8bf1b2163c7267550
Wheely <100705478+Wheelee@users.noreply.github.com>
2024-02-16 16:31:42 +0100

    up

--- src/app/Details/page.jsx
+++ src/app/Details/page.jsx
@@ -1,6 +1,50 @@
+'use client'
+import { motion } from "framer-motion"
+
 const DetailsPage = () => {
-return (
-    <div classname=''>Détails</div>
+    return (
+        <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
+            {/*CONTAINER*/}
+            <div className="">
+                {/*TEXT CONTAINER*/}
+                <div className="p-4 sm:p-8 md:p-12 lg:px-20 xl:p-48 pt-6 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+                    {/*BIOGRAPHY*/}
+                    <div className="flex flex-col gap-12 justify-center">
+                        <h1 className="font-bold text-2xl">Biographie</h1>
+                        <p className="text-lg">
+                            Lorem ipsum dolor sit amet consecteur adipisicing elit.
+                            Nostrum harum quibusdam cupiditate nobisaccusamus sed aut aperiam,
+                            reiciendis numquam ! Voluptas voluptatibus obcaecati dolore itaque suspicit ! Vel doloremque quam nihil.
+                        </p>
+                        <span className="italic">
+                            Le Bilbo sera toujours au-dessus du niveau de l'eau...
+                        </span>
+                        <div className="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-big-down-dash"><path d="M15 5H9" /><path d="M15 9v3h4l-7 7-7-7h4V9z" /></svg>
+                        </div>
+                        {/*SKILLS*/}
+                        <div className="flex flex-col gap-12 justify-center">
+                            <h1 className="font-bold text-2xl">Skills</h1>
+                            {/*SKILLS LIST*/}
+                            <div className="flex flex-row w-fit">
+                                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">HTML/CSS</div>
+                                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">Java</div>
+                                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">PHP</div>
+                                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">NextJS</div>
+                                <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">SQL</div>
+                            </div>
+                            {/*SKILLS SCROLL SVG*/}
+                        </div>
+                    </div>
+                    <div className="">
+                        {/*EXPERIENCE*/}
+                        EXPERIENCE
+                    </div>
+                </div>
+                {/*SVG CONTAINER*/}
+                <div className="hidden"></div>
+            </div>
+        </motion.div>
     )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,31 +2,34 @@
 import Image from "next/image"
 import face from "../../public/hero.png"
 import Type from "@/Components/type"
+import { motion } from "framer-motion"
 
 const HomePage = () => {
 return (
-  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-    {/*IMAGE CONTAINER*/}
-    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-      <Image src={face} alt="Visage en art" className="object-contain"/>
-    </div>
-    {/*TEXT CONTAINER*/}
-    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center p-10">
-      {/*TITLE*/}
-      <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
-      {/*DESCRIPTION*/}
-      <p className="md:text-xl flex flex-col">
-        Welcome to my digital portfolio, where innovations and creativity converge. 
-        With a k  een eye for aesthetics ans a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.
-        <Type/>
-      </p>
-      {/*BUTTONS*/}
-      <div className=" flex gap-4 w-full">
-        <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Voir mon travail</button>
-        <button className="p-4 rounded-lg ring-1 ring-black bg-transparent text-black">Me Contacter</button>
+  <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/*IMAGE CONTAINER*/}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src={face} alt="Visage en art" className="object-contain"/>
+      </div>
+      {/*TEXT CONTAINER*/}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center p-10">
+        {/*TITLE*/}
+        <h1 className="text-4xl md:text-6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
+        {/*DESCRIPTION*/}
+        <p className="md:text-xl flex flex-col">
+          Welcome to my digital portfolio, where innovations and creativity converge. 
+          With a k  een eye for aesthetics ans a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.
+          <Type/>
+        </p>
+        {/*BUTTONS*/}
+        <div className=" flex gap-4 w-full">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Voir mon travail</button>
+          <button className="p-4 rounded-lg ring-1 ring-black bg-transparent text-black">Me Contacter</button>
+        </div>
       </div>
     </div>
-  </div>
+  </motion.div>
   )
 }
 
